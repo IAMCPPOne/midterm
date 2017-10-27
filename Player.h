62164a2e38687a9e539674e8650177f96d599113
Karl <karl.statz@gmail.com>
2017-10-26 20:55:37 -0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -4,13 +4,14 @@
 
 #ifndef PROJ_PLAYER_H
 #define PROJ_PLAYER_H
+#include "GameBoard.h"
 
 class Player{
     int _movesRemaining;
 public:
     Player(int movesRemaining);
     int GetMovesRemaining();
-    char * Move(int x, int y);
+    char * Move(int x, int y, GameBoard gameBoard);
     ~Player();
 };
 
